055a3c280af341f4ee83a7c9b2d578d8aecfcb39
watermelonPop <SK21007@Tamu.edu>
2025-02-08 12:27:33 -0600

    fix

--- api/version.js
+++ api/version.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const { neon } = require('@neondatabase/serverless');
-
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
 const sql = neon(`postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?sslmode=require`);
 
 module.exports = async (req, res) => {
